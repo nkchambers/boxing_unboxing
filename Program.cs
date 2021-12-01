57f4756e767beac1b95a0e7ac7acaa12409e416d
nkchambers <nkchames@gmail.com>
2021-11-30 16:09:51 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //---------------------------------------------LIST OF BOXED DATA----------------------------------------
+            //C1 - Instantiate Empty List of Type Object
+            List<object> BoxedData = new List<object>();
+
+            BoxedData.Add(7);
+            BoxedData.Add(28);
+            BoxedData.Add (-1);
+            BoxedData.Add("true");
+            BoxedData.Add ("chair");
+
+
+            
+            //C3 - Loop through list and print out each item in list
+            foreach (var entry in BoxedData) {
+                if (entry is int) {
+                    Console.WriteLine(entry);
+                }
+                if (entry is string) {
+                    Console.WriteLine(entry);
+                }
+            }
+        }
+    }
+}
